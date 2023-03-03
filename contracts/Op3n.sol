6b0ca4ecdb25ee4f36578401f130015f8eef4c82
RevanthGundala <93841932+RevanthGundala@users.noreply.github.com>
2023-03-03 14:23:45 -0700

    first commit

--- contracts/Op3n.sol
+++ contracts/Op3n.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-//error Error__NotNFT(address msg_sender, address nftAddress);
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -15,13 +13,6 @@ contract Op3n is Ownable {
 
     event newMessage(address _user, uint _tokenId, uint time);
     NFT private nft;
-
-    // modifier onlyNft() {
-    //     if (msg.sender != nftAddress) {
-    //         revert Error__NotNFT(msg.sender, nftAddress);
-    //     }
-    //     _;
-    // }
 
     constructor() {
         nft = new NFT();
