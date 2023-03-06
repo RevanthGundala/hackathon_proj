4a706a6c15214f3149da5b1aae6ab73606a772b3
RevanthGundala <93841932+RevanthGundala@users.noreply.github.com>
2023-03-05 18:34:01 -0700

    initial commit

--- client/src/components/MessageListed.js
+++ client/src/components/MessageListed.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function MessageListed(props) {
-    let {from, subject, date, address} = props.message
-    const [action, setAction] = useState('none')
-    function sendAction(event){
-        setAction(event.target.getAttribute('action'));
+  let { from, subject, date, address, content } = props.message;
+  const encodedStr = encodeURIComponent(JSON.stringify(props.message));
+  const [action, setAction] = useState("null");
+  function sendAction(event) {
+    setAction(event.target.getAttribute("action"));
+  }
+  const render = () => {
+    if (action != "null") {
+      return <Navigate to={`/view/${encodedStr}`} />;
     }
-    const render = () => {
-        if (action != "none") {
-            return <Navigate to={`/view/${action}`} />;
-        }
-    };
-
-    return (
-        <div className="listedMessage" onClick={sendAction} action={address} >
-            <div>{subject}</div>
-            <div>{from}</div>
-            <div>{date}</div>
-            {render()}
-        </div>
-    );
+  };
+  return (
+    <div className="listedMessage" onClick={sendAction} action={encodedStr}>
+      <div>{from}</div>
+      <div>{subject}</div>
+      <div>{date}</div>
+      {render()}
+    </div>
+  );
 }
 
 export default MessageListed;
